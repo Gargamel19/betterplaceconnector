0181ce850edb9a265f0b38fde040c072daa271f7
Ferdinand <trendelenburger19.04@googlemail.com>
2022-02-28 14:30:27 +0100

    added vars

--- start.py
+++ start.py
@@ -6,25 +6,16 @@ import os
 app = Flask(__name__)
 
 server = os.environ.get("SERVER")
-print(server)
 port = int(os.environ.get("PORT_SERVER"))
-print(port)
 nickname = os.environ.get("NICKNAME")
-print(nickname)
 token = os.environ.get("TOKEN")
-print(token)
 channel = os.environ.get("CHANEL")
-print(channel)
+
 sock = socket.socket()
-print("socket")
 sock.connect((server, port))
-print("connect")
 sock.send(f"PASS {token}\n".encode('utf-8'))
-print("PASS")
 sock.send(f"NICK {nickname}\n".encode('utf-8'))
-print("NICK")
 sock.send(f"JOIN {channel}\n".encode('utf-8'))
-print("JOIN")
 print("logged in")
 
 
@@ -39,7 +30,7 @@ def donation():
     cents = int(json_payload['donation']['amount_in_cents'])
     message = json_payload['donation']['comment']
     name = json_payload['donation']['donor_display_name']
-    sock.send(f"PRIVMSG {channel} :[Spende] {name} spendet {str(cents/100)} Euro: {message}\n".encode('utf-8'))
+    sock.send(f"PRIVMSG {channel} :!tts {name} spendet {str(cents/100)} Euro: {message}\n".encode('utf-8'))
     return "okay"
 
 if __name__ == '__main__':
