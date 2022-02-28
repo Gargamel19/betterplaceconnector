5239e39ac7ac5bfa2153db9064a86ad0c914f74e
Ferdinand <trendelenburger19.04@googlemail.com>
2022-02-28 01:42:31 +0100

    add requirements file

--- start.py
+++ start.py
@@ -2,14 +2,31 @@ from flask import Flask
 from flask import request
 import socket
 
-app = Flask(__name__)
-sock = None
 
 server = 'irc.chat.twitch.tv'
 port = 6667
 nickname = 'fettarmqp'
 token = 'oauth:h5j4vm7akv4ekgialq7s8hkkwsy262'
 channel = '#fettarmqp'
+
+sock = socket.socket()
+sock.connect((server, port))
+sock.send(f"PASS {token}\n".encode('utf-8'))
+print("PASS")
+sock.send(f"NICK {nickname}\n".encode('utf-8'))
+print("NICK")
+resp = sock.recv(2048).decode('utf-8')
+print(resp)
+sock.send(f"JOIN {channel}\n".encode('utf-8'))
+print("JOIN")
+resp = sock.recv(2048).decode('utf-8')
+print(resp)
+resp = sock.recv(2048).decode('utf-8')
+print(resp)
+print("logged in")
+
+app = Flask(__name__)
+
 
 
 @app.route('/', methods=['GET'])
@@ -28,20 +45,6 @@ def donation():
 
 if __name__ == '__main__':
 
-    sock = socket.socket()
-    sock.connect((server, port))
-    sock.send(f"PASS {token}\n".encode('utf-8'))
-    print("PASS")
-    sock.send(f"NICK {nickname}\n".encode('utf-8'))
-    print("NICK")
-    resp = sock.recv(2048).decode('utf-8')
-    print(resp)
-    sock.send(f"JOIN {channel}\n".encode('utf-8'))
-    print("JOIN")
-    resp = sock.recv(2048).decode('utf-8')
-    print(resp)
-    resp = sock.recv(2048).decode('utf-8')
-    print(resp)
-    print("logged in")
+
 
     app.run()
